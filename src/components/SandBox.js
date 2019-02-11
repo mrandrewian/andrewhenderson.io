5694c79167816841ed529d1f6091e52e55e30a68
Andrew Henderson <mrandrewian@gmail.com>
2019-02-11 00:14:33 -0500

    Updated Sanbox to use cards

--- src/components/SandBox.js
+++ src/components/SandBox.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 import '../styles/ShapeStudy.scss';
 
 class SandBox extends Component {
@@ -9,8 +9,15 @@ class SandBox extends Component {
         <Col className="my-5" >
           <h1>Sandbox</h1>
           <div className="sand-box rounded">
-            <div class="star-12">
-            </div>
+            <Card className="text-dark" style={{ width: '18rem' }}>
+              <div class="star-12 mx-auto mt-5 mb-4" />
+              <Card.Body>
+                <Card.Title>Spinning 12pt Star</Card.Title>
+                <Card.Text>
+                  Using css animations and overlapping psuedo element styling you can easily achieve this "usefull" little star.
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </div>
         </Col>
       </Row>
